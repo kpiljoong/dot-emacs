81fd415a20cd22f31d2caf6e36f6e49660c5032f
piljoong <kpiljoong@gmail.com>
2017-05-23 03:25:29 +0900

    modify evil

--- init.el
+++ init.el
@@ -68,18 +68,3 @@
 
 ;; babeling org files
 (hkk/babel-org-files)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (color-theme-sanityinc-tomorrow use-package which-key bind-key bind-map evil undo-tree goto-chg))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underilne nil)))))
